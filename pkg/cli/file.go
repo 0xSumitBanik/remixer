5bad6911bacdd226a68fdde644aca4fe8681badb
Sumit Banik <sumitbanik02@gmail.com>
2022-06-27 11:36:57 +0530

    [add] check file extension #2

--- pkg/cli/file.go
+++ pkg/cli/file.go
@@ -9,11 +9,16 @@ import (
 
 func ProcessFileInput(filePath string) (string, error) {
 	if filePath != "" {
-		fileContent, err := utils.ReadFileContent(filePath)
-		config.CheckError(err)
-
-		base64Encoded := utils.Base64Encode(fileContent)
-		return base64Encoded, nil
+		if matchedExtension := utils.CheckFileExtension(filePath,".sol") ; matchedExtension {
+			fileContent, err := utils.ReadFileContent(filePath)
+			config.CheckError(err)
+	
+			base64Encoded := utils.Base64Encode(fileContent)
+			return base64Encoded, nil
+		} else {
+			err := errors.New("input file must be a .sol file")
+			return "", err
+		}
 	} else {
 		err := errors.New("specify a filename to generate shareable link")
 		return "", err
